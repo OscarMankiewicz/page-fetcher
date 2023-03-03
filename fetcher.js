44d2c74f84095e95ed49a0bd45c5019ef1311239
Oscar <omankiewicz77@gmail.com>
2023-03-03 14:24:10 -0500

    incorportated error and response on the request function

--- fetcher.js
+++ fetcher.js
@@ -6,3 +6,13 @@ const readline = require('readline');
 //aquire info from from cmd line
 const url = process.argv[2];
 const filePath = process.argv[3];
+
+//make a request to the url
+request(url, (error, response, body) => {
+    if (error) {
+        //log an error
+        console.error(`Error: ${error}`)
+    } else if  (response.statusCode !== 200) {
+        console.error(`Status code: ${response.statusCode}`)
+    }
+})
