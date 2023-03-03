7b6b796acc6780ba96dfa272251ef083f50dbedd
Oscar <omankiewicz77@gmail.com>
2023-03-03 15:19:25 -0500

    removed useless require function

--- fetcher.js
+++ fetcher.js
@@ -1,7 +1,6 @@
 //import functions from node.js
 const fs = require('fs');
 const request = require('request');
-const readline = require('readline');
 
 //aquire info from from cmd line
 const url = process.argv[2];
